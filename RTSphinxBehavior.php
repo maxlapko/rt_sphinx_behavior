649bcf451121f6168805cbdf0aacb159f2c535fb
Max Lapko <maxlapko@gmail.com>
2012-06-03 14:07:03 +0300

    init commit

---
+++ RTSphinxBehavior.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * RTSphinxBehavior
+ *
+ * @author mlapko
+ */
+class RTSphinxBehavior extends CActiveRecordBehavior
+{
+    /**
+     * Method for getting index data
+     * 
+     * @var mixed 
+     */
+    public $getDataMethod;
+    
+    /**
+     * Sphinx table
+     * 
+     * @var string 
+     */
+    public $sphinxTable = 'rt_table';
+    
+    /**
+     * Sphinx db component name or component instance
+     * 
+     * @var mixed 
+     */
+    public $sphinxDbComponent = 'sphinxDb';
+    
+    /**
+     * Enable or disable callbacks
+     * 
+     * @var boolean 
+     */
+    public $allowCallbacks = true;
+    
+    /**
+     * Enable or disable behavior
+     *  
+     * @var boolean
+     */
+    public $disabled = false;
+    
+    /**
+     *
+     * @var SphinxDbCommand 
+     */
+    protected $_command;
+
+    /**
+     * @return SphinxDbCommand
+     */
+    public function getCommand($sql = null)
+    {
+        if ($this->_command === null) {
+            $conn = is_object($this->sphinxDbComponent) ? $this->sphinxDbComponent : Yii::app()->{$this->sphinxDbComponent};
+            $conn->setActive(true);
+            $this->_command = new SphinxDbCommand($conn);            
+        }
+        return $this->_command->setText($sql);
+    }
+    
+    /**
+     * Insert index into sphinx
+     * @param mixed $data
+     * @return boolean 
+     */
+    public function insertIndex($data = null)
+    {        
+        if ($this->disabled) {
+            return true;
+        }        
+        if ($data === null) {
+            $data = call_user_func($this->getDataMethod);            
+        }       
+        return $this->getCommand()->insert($this->sphinxTable, $data);
+    }
+    
+    /**
+     * Update sphinx index
+     * 
+     * @param mixed $data
+     * @return boolean 
+     */
+    public function updateIndex($data = null)
+    {
+        if ($this->disabled) {
+            return true;
+        }
+        if ($data === null) {
+            $data = call_user_func($this->getDataMethod);            
+        }
+        return $this->getCommand()->replace($this->sphinxTable, $data);
+    }
+    
+    /**
+     * Delete index from sphinx
+     * @param null|array $ids 
+     * @return integer
+     */
+    public function deleteIndex($ids = null)
+    {
+        if ($this->disabled) {
+            return true;
+        }
+        if ($ids === null) {
+            $ids = array($this->getOwner()->getPrimaryKey());            
+        } elseif (!is_array($ids)) {
+            throw new CException('The param "ids" should be array.');
+        }
+        
+        if (count($ids) > 0) {
+            return $this->getCommand()->delete($this->sphinxTable, 'id IN (' . implode(',', $ids) . ')');
+        }
+        return 0;
+    }
+    
+    /**
+     * Responds to {@link CModel::onAfterSave} event.
+     * Inserted or updated sphinx index
+     *
+     * @param CModelEvent $event event parameter
+     */
+    public function afterSave($event)
+    {
+        if (!$this->allowCallbacks) {
+            return true;
+        }
+        
+        if ($this->getOwner()->getIsNewRecord()) {
+            $this->insertIndex();
+        } else {
+            $this->updateIndex();
+        }        
+    }
+    
+    /**
+     * Responds to {@link CModel::onAfterDelete} event.
+     * Inserted or updated sphinx index
+     *
+     * @param CModelEvent $event event parameter
+     */
+    public function afterDelete($event)
+    {
+        if (!$this->allowCallbacks) {
+            return true;
+        }
+        $this->deleteIndex();    
+    }
+    
+}
+
+
+class SphinxDbCommand extends CDbCommand
+{
+    const INSERT_COMMAND = 'INSERT';
+    const REPLACE_COMMAND = 'REPLACE';
+    
+    /**
+     * Creates and executes an INSERT SQL statement.
+     * The method will properly escape the column names, and bind the values to be inserted.
+     * @param string $table the table that new rows will be inserted into.
+     * @param array $columns the column data (name=>value) to be inserted into the table.
+     * @return integer number of rows affected by the execution.
+     */
+    public function insert($table, $columns)
+    {
+        return $this->_intoCommand($table, $columns, self::INSERT_COMMAND);
+    }
+    
+    /**
+     * Creates and executes an REPLACE SQL statement.
+     * The method will properly escape the column names, and bind the values to be replaced.
+     * @param string $table the table that new rows will be inserted into.
+     * @param array $columns the column data (name=>value) to be inserted into the table.
+     * @return integer number of rows affected by the execution.
+     */
+    public function replace($table, $columns)
+    {
+        return $this->_intoCommand($table, $columns, self::REPLACE_COMMAND);
+    }    
+    
+    protected function _intoCommand($table, $columns, $type = self::INSERT_COMMAND)
+    {
+        $params=array();
+        $names = array();
+        $placeholders = array();
+        foreach ($columns as $name => $value) {
+            $names[] = $this->getConnection()->quoteColumnName($name);
+            if ($value instanceof CDbExpression) {
+                $placeholders[] = $value->expression;
+                foreach ($value->params as $n => $v)
+                    $params[$n] = $v;
+            } else {
+                $placeholders[] = ':' . $name;
+                $params[':' . $name] = $value;
+            }
+        }
+        $sql = $type . ' INTO ' . $this->getConnection()->quoteTableName($table)
+            . ' (' . implode(', ', $names) . ') VALUES ('
+            . implode(', ', $placeholders) . ')';
+        return $this->setText($sql)->execute($params);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,70 @@
+Real Time Sphinx behavior for Yii 
+
+## Getting started
+
+Put files to protected/components or extensions directory
+
+### Configuring Model
+
+```php
+/**
+ * This is the model class for table "posts".
+ *
+ * The followings are the available columns in table 'posts':
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ */
+class Post extends CActiveRecord
+{
+    // ....... 
+    
+    public function behaviors()
+    {
+        return array(
+            'RTSphinxBehavior' => array(
+                'class'             => 'ext.rt_sphinx_behavior.RTSphinxBehavior'
+                'getDataMethod'     => array($this, 'getIndexData'),
+                'sphinxTable'       => 'rt_posts',
+                'sphinxDbComponent' => 'sphinxDbComponent', // component name or Yii::app()->sphinxDbComponent
+                'allowCallbacks'    => true,
+                'disabled'          => false, // on or off 
+            ),
+        );
+    }
+
+    // .....
+    /**
+     * @return array
+     */
+    public function getIndexData()
+    {       
+        return $this->getAttributes(); // or custom query        
+    }
+
+}
+
+// custom use
+
+if (($post = Post::model()->findByPk(1)) !== null) {
+    $post->insertIndex(); // uses getDataMethod
+    // or
+    $post->updateIndex(); // uses getDataMethod
+    // or
+    $post->deleteIndex(); // uses owner pk
+}
+
+// or insert index
+for ($indexes as $indexData) 
+{
+    Post::model()->insertIndex($indexData);
+    // or
+    Post::model()->replaceIndex($indexData);
+}
+
+// or multiple delete
+Post::model()->deleteIndex(array(1, 2, 4, 3 ,5));
+
+
+
+```
